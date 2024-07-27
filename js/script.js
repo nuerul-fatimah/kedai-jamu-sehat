4a36f34135d2831e57a2cdaaef5ac6ae235b8893
nuerul-fatimah <fat9413519@gmail.com>
2024-07-27 22:46:30 +0700

    kedai-jamu-sehat

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// toogle clas active
+const navbarNav = document.querySelector('.navbar-nav');
+// ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+navbarNav.classList.toggle('active');
+};
+
+// klik diluarvsidebar untuk menghilangkan nav
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e) {
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target))
+        navbarNav.classList.remove('active');
+});
